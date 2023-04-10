30d3bd8bfcfbfcb83e2a62b6e8f64543d0cb3175
Ilias Lourentzakis <iliaslourentzakis@gmail.com>
2023-04-10 21:40:52 +0300

    added generic parameter

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Pokemon } from '../api/PokemonApi/types';
 
-export const useLocalStorage = (key: string, defaultValue: Array<Pokemon>) => {
+export const useLocalStorage = <T>(key: string, defaultValue: Array<T>) => {
   const [value, setValue] = useState(() => {
     if (localStorage.getItem(key) === null) return defaultValue;
     return JSON.parse(localStorage.getItem(key)!);
