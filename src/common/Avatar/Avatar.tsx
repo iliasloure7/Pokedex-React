053721b060450e5b039aafa5308f6f0099eb6d9e
Ilias Lourentzakis <iliaslourentzakis@gmail.com>
2023-04-16 18:56:00 +0300

    added a click handler to close the sidebar

--- src/common/Avatar/Avatar.tsx
+++ src/common/Avatar/Avatar.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { extractIdFromUrl } from '../../utils';
 import { Paths } from '../../constants';
 import { Pokemon } from '../../api/PokemonApi/types';
+import { useSidebarContext } from '../../context/SidebarProvider/SidebarProvider';
 
 type Props = {
   pokemon: Pokemon;
@@ -9,10 +10,15 @@ type Props = {
 };
 
 function Avatar({ pokemon, size = 'w-auto' }: Props) {
+  const { setToggleSidebar } = useSidebarContext();
   const pokemonId = extractIdFromUrl(pokemon.url);
 
   return (
-    <Link to={`${Paths.DETAILS}/${pokemonId}`} draggable={false}>
+    <Link
+      to={`${Paths.DETAILS}/${pokemonId}`}
+      draggable={false}
+      onClick={() => setToggleSidebar(false)}
+    >
       <img
         className={`rounded-t-lg bg-gray-200 block mx-auto ${size}`}
         src={`${import.meta.env.VITE_POKEMON_API_IMAGE_URL}/${pokemonId}.png`}
